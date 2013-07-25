72a8ca6dd9fbf91bac3ec30c1659a6650b82f115
Hamdi LAADHARI <killerwolf06@gmail.com>
2013-07-25 13:32:07 -0400

    API request are handled by a third party object that should implement HTTPClientInterface fixes #1

--- src/JCDodatawrapper/Vls/Wrapper.php
+++ src/JCDodatawrapper/Vls/Wrapper.php
@@ -41,55 +41,12 @@ class Wrapper{
         return $this->_query( sprintf( $this->operations[2]['query'],$station_id, $contract_name , $this->apiKey ) );
     }
     
+    public function setHTTPClient( $client )
+    {
+        $this->HTTPClient = $client;
+    }
     private function _query( $url )
     {
-        $url = self::API_ENDPOINT . $url;
-        // First try CURL
-        if ( extension_loaded( 'curl' ) )
-        {
-            $ch = curl_init( $url );
-            // Options used to perform in a similar way than PHP's fopen()
-            curl_setopt_array(
-                $ch,
-                array(
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_SSL_VERIFYPEER => false
-                )
-            );
-
-            // Getting data
-            ob_start();
-            if ( !curl_exec( $ch ) )
-            {
-                curl_close( $ch );
-                ob_end_clean();
-                return false;
-            }
-
-            curl_close ( $ch );
-            $data = ob_get_contents();
-            ob_end_clean();
-
-            return json_decode( $data );
-        }
-
-        // Open and read url
-        $fid = fopen( $url, 'r' );
-        if ( $fid === false )
-        {
-            return false;
-        }
-
-        $data = "";
-        do
-        {
-            $dataBody = fread( $fid, 8192 );
-            if ( strlen( $dataBody ) == 0 )
-                break;
-            $data .= $dataBody;
-        } while( true );
-
-        fclose( $fid );
-        return json_decode( $data );
+        return $this->HTTPClient->get( self::API_ENDPOINT . $url );
     }
 }
